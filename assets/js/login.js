b10a1ddcf53178a0f73cf1cb71c03ce3b746becb
xhz <531035705@qq.com>
2023-03-27 04:34:29 +0800

    添加了登录注册功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,4 +8,63 @@ $(function(){
         $('.reg-box').hide();
         $('.log-box').show();
     })
+
+    /* 从layui中获取到form对象 */
+    var form = layui.form;
+    form.verify({
+        pass: [
+            /^[\S]{6,12}$/
+            ,'密码必须6到12位，且不能出现空格'
+          ] ,
+          repwd: function(value) {
+            // 通过形参拿到的是确认密码框中的内容
+            // 还需要拿到密码框中的内容
+            // 然后进行一次等于的判断
+            // 如果判断失败,则return一个提示消息即可
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+              return '两次密码不一致！'
+            }
+          }
+    })
+
+
+    $('#form_reg').on('submit', function(e) {
+        // 1. 阻止默认的提交行为
+        e.preventDefault()
+        // 2. 发起Ajax的POST请求
+        var data = {
+          username: $('#form_reg [name=username]').val(),
+          password: $('#form_reg [name=password]').val()
+        }
+        $.post('/api/reguser', data, function(res) {
+          if (res.status !== 0) {
+            return layer.msg(res.message)
+          }
+
+         layer.msg("注册成功，请登录")
+         $('#link_log').click();
+        })
+      })
+
+    $('#form_log').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            url:'/api/login',
+            method:'POST',
+            data:$(this).serialize(),
+            success:function(res){
+                if(res.status !== 0){
+                  return  layer.msg(res.message)
+                }
+                layer.msg("登陆成功！")
+
+                localStorage.setItem('token',res.token);
+                location.href = '/index.html'
+
+                
+            }
+        }
+        )
+    })
 })
